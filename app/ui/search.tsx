79812012549e42cec884847ea45361d1af7d086a
pw3nuts <paras.w3nuts@gmail.com>
2024-09-05 20:29:42 +0530

    changes for addd dashboard and login

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -16,6 +16,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         console.log(term);
       }
       const params = new URLSearchParams(searchParams);
+      params.set('page', '1');
       if (term) {
         params.set('query', term);
       } else {
@@ -32,7 +33,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-        placeholder={placeholder} onChange={(e) => {
+        placeholder={placeholder} 
+        onChange={(e) => {
           handleSearch(e.target.value);
         }}
         defaultValue={searchParams.get('query')?.toString()}
